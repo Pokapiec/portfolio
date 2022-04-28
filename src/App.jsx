18a647771446d837da902133d7d9b64ac16e1c99
Pokapiec <pokapiec@gmail.com>
2022-04-28 22:06:09 +0200

    Fix fckn git :)

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,25 @@
-import { useState } from 'react'
-import styled from "styled-components";
-// import './App.css'
+import './Main.scss'
+import Navbar from './Components/Navbar/Navbar';
+import Header from './Components/Header/Header';
+import About from './Components/About/About';
+import Tech from './Components/Tech/Tech';
+import Contact from './Components/Contact/Contact';
+import Socials from './Components/Socials/Socials';
+import Projects from './Components/Projects/Projects';
 
-const MainDiv = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  border: 1px solid black;
-`;
 
 function App() {
 
   return (
-    <>
-      <header>
-        <h1>Paweł Okapiec</h1>
-        <h5>Web developer | Data engenieer</h5>
-      </header>
-
-      <section>
-        <h3>About me</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit id iste minus tenetur harum, nihil placeat ullam sed. Earum nobis culpa delectus molestiae deleniti similique magnam dignissimos aliquam perspiciatis voluptas esse eveniet nemo corrupti magni exercitationem consequuntur fugiat, provident quisquam qui minima, quo error optio omnis iste? Eaque, quisquam ipsam!</p>
-      </section>
-
-      <section>
-        <h3>My focusses</h3>
-        <ul>
-          <li>React</li>
-          <li>Django</li>
-          <li>Flask</li>
-          <li>FastAPI</li>
-        </ul>
-      </section>
-
-      <main>
-        <h3>My work</h3>
-        <ul>
-          <li>Experience a gain at work</li>
-          <li>Project 1</li>
-          <li>Project 2</li>
-          <li>Project 3</li>
-          <li>Project 4</li>
-        </ul>
-      </main>
-    </>
+    <div className='container'>
+      <Navbar/>
+      <Header/>
+      <About/>
+      <Tech/>
+      <Projects/>
+      <Contact/>
+      <Socials/>
+    </div>
   )
 }
 
